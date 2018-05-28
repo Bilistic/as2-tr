dcdd507e1f9f89b37eaee06737ea3e2e1ed521cd
Bilistic <joshua.nuttall@mycit.ie>
2018-05-28 04:51:10 +0100

    Add files via upload

---
+++ test/test_server.py
@@ -0,0 +1,20 @@
+from server.Server import DataAnalysis
+import pytest
+
+"""
+tests sentiment analysis on server.
+"""
+def test_sentiment():
+    obj = {'length': 80, 'method': 'reddit',
+           'text': 'I m A Democrat And The Left s Russia Gaslighting Scares Me More Than Trump Does'}
+    ans = DataAnalysis.analyze_sentiment(obj)
+    assert int(ans.sentiment) == 0.25
+
+
+"""
+tests cleaning text on server.
+"""
+def test_twitter():
+    sentence = "[Dirty-] sentence that (/needs/) {cleaning}"
+    ans = DataAnalysis.clean(sentence)
+    assert ans == "Dirty sentence that needs cleaning"
