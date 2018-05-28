d003630a2494b0d249a9bf9472119ebf112cf343
Bilistic <joshua.nuttall@mycit.ie>
2018-05-28 04:55:03 +0100

    Update test_server.py

--- test/test_server.py
+++ test/test_server.py
@@ -7,7 +7,7 @@ tests sentiment analysis on server.
 def test_sentiment():
     obj = {'length': 80, 'method': 'reddit',
            'text': 'I m A Democrat And The Left s Russia Gaslighting Scares Me More Than Trump Does'}
-    ans = DataAnalysis.analyze_sentiment(obj)
+    ans = DataAnalysis().analyze_sentiment(obj)
     assert int(ans.sentiment) == 0.25
 
 
@@ -16,5 +16,5 @@ tests cleaning text on server.
 """
 def test_twitter():
     sentence = "[Dirty-] sentence that (/needs/) {cleaning}"
-    ans = DataAnalysis.clean(sentence)
+    ans = DataAnalysis().clean(sentence)
     assert ans == "Dirty sentence that needs cleaning"
